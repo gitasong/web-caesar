c188f26177ee5293924750a14718f7071af9a0ac
Nicole Freed <nicole.freed@gmail.com>
2017-06-17 22:38:22 -0700

    add string formatting so encrypted message stays in textarea after encryption

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
@@ -31,7 +31,7 @@ form = """
       <label for="rot">Rotate by:
         <input type="text" name="rot" value="0" />
       </label>
-        <textarea name="text" />Enter your message here.</textarea>
+        <textarea name="text" />{0}</textarea>
         <input type="submit" value="Encrypt Me!"/>
     </body>
 </html>
@@ -46,11 +46,11 @@ def encrypt():
 
     # renders encrypted message in browser
     encrypted_message = "<h1>" + encrypted_string + "</h1>"
-    return encrypted_message
+    return form.format(encrypted_string)
 
 
 @app.route("/")
 def index():
-    return form
+    return form.format("")
 
 app.run()
