314e719ccf555577d8fb58487e8265a9a9a68880
Nicole Freed <nicole.freed@gmail.com>
2017-06-17 22:15:06 -0700

    add caesar.py, encrypt function, and rendering of encrypted message

--- main.py
+++ main.py
@@ -1,10 +1,56 @@
-from flask import Flask
+from flask import Flask, request
+from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+      <!-- create your form here -->
+      <form action="/" method="post">
+      <label for="rot">Rotate by:
+        <input type="text" name="rot" value="0" />
+      </label>
+        <textarea name="text" />Enter your message here.</textarea>
+        <input type="submit" value="Encrypt Me!"/>
+    </body>
+</html>
+"""
+
+# encrypt function
+@app.route("/", methods=['POST'])
+def encrypt():
+    rot = int(request.form['rot'])
+    text = request.form['text']
+    encrypted_string = rotate_string(text, rot)
+
+    # renders encrypted message in browser
+    encrypted_message = "<h1>" + encrypted_string + "</h1>"
+    return encrypted_message
+
+
 @app.route("/")
 def index():
-    return "Hello World"
+    return form
 
 app.run()
